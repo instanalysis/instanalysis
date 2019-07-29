792c54a69efe066c86ce8a54cdc2f2daa77774c9
yploekito <yp.loekito@gmail.com>
2019-07-29 10:02:39 +0700

    merged with amazon rekognition branch

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,5 +1,5 @@
 const personalityAnalysis = require('../helper/personalityAnalysis')
-const {faceDetection} = require('../helper/amazonRekognition')
+const {faceDetection, labelDetection} = require('../helper/amazonRekognition')
 class analysisController{
     static async analysis (req,res){
         try{
@@ -8,13 +8,134 @@ class analysisController{
                 posts: [
                     {
                         imageURL: 'https://cbsnews1.cbsistatic.com/hub/i/2018/11/06/0c1af1b8-155a-458e-b105-78f1e7344bf4/2018-11-06t054310z-1334124005-rc1be15a8050-rtrmadp-3-people-sexiest-man.jpg',
-                        caption: 'Happiest birthday to @potatoccalon 🎉🎉 wishing you bestest things in life and lots of inspiration and happy moments this year 💖💖💖(*´∇｀*)⭐️⭐️⭐️ #bnha #bokunoheroacademia #mha #myheroacademia #bakugoukatsuki #bakugou #kacchan #chibi #bnhafanart',
+                        caption: 'Unplug and unwind',
                         likes: 2842,
                         date: '2017'
                     },
                     {
                         imageURL: 'https://s.abcnews.com/images/GMA/190416_gma_digital_garner_hpMain_16x9_992.jpg',
-                        caption: '⭐️CHIBI COMMISSIONS OPEN⭐️ I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku  I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku  I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku  I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku  I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku  I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku',
+                        caption: 'If monday can be fun, Wae Rebo? #kenapaya?',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                        caption: 'Clubstyle ✌️ #fxdc #clubstyle #dyna',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'One of those places where you’d feel so young 👨🏻👵🏻',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                        caption: 'Iceland is petrifying.',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://s.abcnews.com/images/GMA/190416_gma_digital_garner_hpMain_16x9_992.jpg',
+                        caption: '#family',
+                        likes: 2369,
+                        date:'2017'
+                    },
+                    {
+                        imageURL: 'https://s.abcnews.com/images/GMA/190416_gma_digital_garner_hpMain_16x9_992.jpg',
+                        caption: 'Grateful to have the chance to play in this hard-fought match along with these monsters. Jakarta Komodos is the champion of 2018 Jak XV 🏉 League',
+                        likes: 2369,
+                        date:'2017'
+                    },{
+                        imageURL: 'https://s.abcnews.com/images/GMA/190416_gma_digital_garner_hpMain_16x9_992.jpg',
+                        caption: 'One of those guud ol days 🌿🐍 #2012',
+                        likes: 2369,
+                        date:'2017'
+                    },{
+                        imageURL: 'https://s.abcnews.com/images/GMA/190416_gma_digital_garner_hpMain_16x9_992.jpg',
+                        caption: 'Rugby. 3 months in, still a burden to the club. @jakarta_komodos 1st game at Jak XV, 📸: @fijianinasia 🍻',
+                        likes: 2369,
+                        date:'2017'
+                    },{
+                        imageURL: 'https://s.abcnews.com/images/GMA/190416_gma_digital_garner_hpMain_16x9_992.jpg',
+                        caption: 'I miss feeling cold behind all the layers of clothing 🎄',
+                        likes: 2369,
+                        date:'2017'
+                    },{
+                        imageURL: 'https://s.abcnews.com/images/GMA/190416_gma_digital_garner_hpMain_16x9_992.jpg',
+                        caption: 'Game of hooligans played by gentlemen.',
+                        likes: 2369,
+                        date:'2017'
+                    },{
+                        imageURL: 'https://s.abcnews.com/images/GMA/190416_gma_digital_garner_hpMain_16x9_992.jpg',
+                        caption: 'Youre such a Houdini but you dont know what youre running away from 🏃🏿',
                         likes: 2369,
                         date:'2017'
                     }
@@ -23,33 +144,62 @@ class analysisController{
             let promiseListFaceDetection = mockData.posts.map((item)=>{
                 return faceDetection(item.imageURL)
             })
-
+            let promiseLabelDetection = mockData.posts.map((item)=>{
+                return labelDetection(item.imageURL)
+            })
             console.log('processing')
             let resultFaceDetection = await Promise.all(promiseListFaceDetection)
+            let resultLabelDetection = await Promise.all(promiseLabelDetection)
+            let interestFromPosts = {}
+            let emotionFromPosts = {}
+            let counter = 0
+            resultFaceDetection.forEach((item, index)=>{
+                if(item.FaceDetails[0]){
+                    counter ++
+                    console.log('hei')
+                    for(let i = 0; i < item.FaceDetails[0].Emotions.length; i++){
+                        if(emotionFromPosts[item.FaceDetails[0].Emotions[i].Type]){
+                            emotionFromPosts[item.FaceDetails[0].Emotions[i].Type] += item.FaceDetails[0].Emotions[i].confidence
+                        }else{
+                            emotionFromPosts[item.FaceDetails[0].Emotions[i].Type] = item.FaceDetails[0].Emotions[i].confidence
+                        }
+                        if(resultFaceDetection.length === index - 1){
+                            emotionFromPosts[item.FaceDetails[0].Emotions[i].Type] = emotionFromPosts[item.FaceDetails[0].Emotions[i].Type]/counter
+                        }
+                    }
+                }
+            })
+            resultLabelDetection.forEach(item=>{
+                if(item.Labels[0].Name !== 'Human' && item.Labels[0].Name !== 'Person' && item.Labels[0].Name !== 'Face'){
+                    if(interestFromPosts[item.Labels[0].Name]){
+                        interestFromPosts[item.Labels[0].Name] += 1
+                    }else{
+                        interestFromPosts[item.Labels[0].Name] = 1
+                    }
+                }
+            })
             const personalityAnalysisResult = await personalityAnalysis(mockData)
             let perPost = mockData.posts.map((item, index)=>{
                 return{
                     faceDetection:resultFaceDetection[index],
+                    labelDetection:resultLabelDetection[index],
                     likes:item.likes,
                     date:item.date,
                 }
-            
-        })
-        let {IBMAnalysis} = converDataStructure(mockData)
-        personalityAnalysis(IBMAnalysis)
-            .then(response=>{
-                console.log(response)
             })
             // console.log(resultFaceDetection)
             console.log('done')
             res.json({
                 username: mockData.username,
                 personalityAnalysisResult,
+                emotionFromPosts,
+                interestFromPosts,
                 perPost
             })
 
         }
         catch(e){
+            console.log(e)
             res.status(500).json(e)
         }   
            
