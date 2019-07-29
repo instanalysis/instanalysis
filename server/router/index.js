98e82308023b80b9bc992dc7c47ff0ab9cf1e32e
stefkwan <stefanikwan@gmail.com>
2019-07-29 13:34:17 +0700

    translate to english middleware ok

--- server/router/index.js
+++ server/router/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const controller = require('../controller')
-
+const translateToEnglish = require('../middleware/translateToEnglish').translateToEnglish
 
 router.get('/', (req,res)=>{
-    res.status(200).json('Hello World, I am Instanalysis')
+    res.status(200).json('Hello World, I am Instanalysiss')
 })
 
-router.get('/analysis', controller.analysis)
+router.get('/analysis', translateToEnglish, controller.analysis)
 
 module.exports = router
