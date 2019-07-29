5dfc65d44ac2f702c5587222956146d1dc1a7908
yploekito <yp.loekito@gmail.com>
2019-07-29 10:01:14 +0700

    oke

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,31 +1,58 @@
 const personalityAnalysis = require('../helper/personalityAnalysis')
-const converDataStructure = require('..//helper/dataToObjectCreator')
+const {faceDetection} = require('../helper/amazonRekognition')
 class analysisController{
-    static analysis(req,res){
-        let mockData = {
-            username: 'viryse',
-            posts: [
-                {
-                    imageURL: 'https://scontent-sin6-2.cdninstagram.com/vp/96f6483c89d170230952fa30e889153e/5DE48A90/t51.2885-15/e35/p1080x1080/65776990_634027807092683_1723135104297470983_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&_nc_cat=111',
-                    caption: 'Happiest birthday to @potatoccalon 🎉🎉 wishing you bestest things in life and lots of inspiration and happy moments this year 💖💖💖(*´∇｀*)⭐️⭐️⭐️ #bnha #bokunoheroacademia #mha #myheroacademia #bakugoukatsuki #bakugou #kacchan #chibi #bnhafanart',
-                    likes: 2842
-                },
-                {
-                    imageURL: 'https://scontent-sin6-2.cdninstagram.com/vp/732bac83b411cacf810389a5f190f659/5DDF53E6/t51.2885-15/e35/s1080x1080/65961529_167550814279802_7613849013870915224_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&_nc_cat=102',
-                    caption: '⭐️CHIBI COMMISSIONS OPEN⭐️ I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku',
-                    likes: 2369
+    static async analysis (req,res){
+        try{
+            let mockData = {
+                username: 'viryse',
+                posts: [
+                    {
+                        imageURL: 'https://cbsnews1.cbsistatic.com/hub/i/2018/11/06/0c1af1b8-155a-458e-b105-78f1e7344bf4/2018-11-06t054310z-1334124005-rc1be15a8050-rtrmadp-3-people-sexiest-man.jpg',
+                        caption: 'Happiest birthday to @potatoccalon 🎉🎉 wishing you bestest things in life and lots of inspiration and happy moments this year 💖💖💖(*´∇｀*)⭐️⭐️⭐️ #bnha #bokunoheroacademia #mha #myheroacademia #bakugoukatsuki #bakugou #kacchan #chibi #bnhafanart',
+                        likes: 2842,
+                        date: '2017'
+                    },
+                    {
+                        imageURL: 'https://s.abcnews.com/images/GMA/190416_gma_digital_garner_hpMain_16x9_992.jpg',
+                        caption: '⭐️CHIBI COMMISSIONS OPEN⭐️ I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku  I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku  I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku  I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku  I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku  I’ll be opening 5 slots for now, if you’re interested please dm me with the number of characters and good reference images! （＾◇＾）I’ll reply when I’m ready to take your commission 💖 sharing is appreciated~.#commissionsopen #commission #artcommission #chibi #bnhafanart #bnha #bokunoheroacademia #mha #myheroacademia #midoriyaizuku #deku',
+                        likes: 2369,
+                        date:'2017'
+                    }
+                ]
+            }
+            let promiseListFaceDetection = mockData.posts.map((item)=>{
+                return faceDetection(item.imageURL)
+            })
+
+            console.log('processing')
+            let resultFaceDetection = await Promise.all(promiseListFaceDetection)
+            const personalityAnalysisResult = await personalityAnalysis(mockData)
+            let perPost = mockData.posts.map((item, index)=>{
+                return{
+                    faceDetection:resultFaceDetection[index],
+                    likes:item.likes,
+                    date:item.date,
                 }
-            ]
-        }
+            
+        })
         let {IBMAnalysis} = converDataStructure(mockData)
         personalityAnalysis(IBMAnalysis)
             .then(response=>{
                 console.log(response)
             })
-            .catch((e)=>{
-                // console.log(e)
-                res.status(500).json(e)
+            // console.log(resultFaceDetection)
+            console.log('done')
+            res.json({
+                username: mockData.username,
+                personalityAnalysisResult,
+                perPost
             })
+
+        }
+        catch(e){
+            res.status(500).json(e)
+        }   
+           
     }
 }
 module.exports= analysisController
