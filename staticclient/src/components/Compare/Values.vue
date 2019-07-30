04c9b3108aff3525beba2b976876491e5d0bd22e
lutfi <mlutfiibra@outlook.com>
2019-07-30 10:44:35 +0700

    Implement likes chart and match page

---
+++ staticclient/src/components/Compare/Values.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="values" class="box">
+    <div class="boxheading">VALUES</div>
+    <p>Shows concern for the wellbeing of others.</p>
+    <p>Embraces change, looks for new experiences.</p>
+    <p>Emphasizes tradition and order.</p>
+    <p>Seeks hedonistic pleasure.</p>
+    <p>Seeks personal success.</p>
+    <div style="width: 29rem; margin: 0 auto;">
+      <values-chart :values="values1"/>
+      <values-chart :values="values2"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ValuesChart from '../ValuesChart';
+
+export default {
+  components: {
+    ValuesChart
+  },
+  data() {
+    return {
+      values1: { // key is from values.trait_id
+				"value_self_transcendence": 0.05015001965356963,
+				"value_openness_to_change": 0.6854512392944407,
+				"value_conservation": 0.02725197430081272,
+				"value_hedonism": 0.3754754766311752,
+				"value_self_enhancement": 0.6890067278325013,
+      },
+      values2: { // key is from values.trait_id
+				"value_self_transcendence": 0.05015001965356963,
+				"value_openness_to_change": 0.6854512392944407,
+				"value_conservation": 0.02725197430081272,
+				"value_hedonism": 0.3754754766311752,
+				"value_self_enhancement": 0.6890067278325013,
+			}
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ staticclient/src/views/Match.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="match">
+    <div class="container">
+      <div class="userprofile" style="justify-content: space-evenly;">
+        <div class="ppc" style="display: flex;">
+          <img class="pp" src="https://scontent-sin2-2.cdninstagram.com/vp/6f793dacb0901845be55c3624ca6bed0/5DD5956B/t51.2885-19/s320x320/59440893_336666376926221_276408968295743488_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com" alt="">
+          <div class="username">zachlavine8</div>
+        </div>
+
+        <div class="ppc">
+          <img class="pp" src="https://scontent-sin2-2.cdninstagram.com/vp/6f793dacb0901845be55c3624ca6bed0/5DD5956B/t51.2885-19/s320x320/59440893_336666376926221_276408968295743488_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com" alt="">
+          <div class="username">zachlavine8</div>
+        </div>
+      </div>
+      <compare-profile style="margin-top: 3rem"></compare-profile>
+      <compare-needs style="margin-top: 2rem"></compare-needs>
+      <compare-values style="margin-top: 2rem"></compare-values>
+    </div>
+  </div>
+</template>
+
+<script>
+import CompareProfile from '@/components/Compare/Profile'
+import CompareNeeds from '@/components/Compare/Needs'
+import CompareValues from '@/components/Compare/Values'
+
+export default {
+  name: 'match',
+  components: {
+    CompareProfile,
+    CompareNeeds,
+    CompareValues
+  }
+}
+</script>
+
+<style>
+
+</style>
