26409b9624589794b2ee67e54f8bb0a842bc9fa5
lutfi <mlutfiibra@outlook.com>
2019-07-30 15:23:51 +0700

    Implement match with mock datum

--- staticclient/src/views/Match.vue
+++ staticclient/src/views/Match.vue
@@ -2,13 +2,17 @@
   <div class="match">
     <div class="container">
       <div class="userprofile" style="justify-content: space-evenly;">
-        <div class="ppc" style="display: flex;">
-          <img class="pp" src="https://scontent-sin2-2.cdninstagram.com/vp/6f793dacb0901845be55c3624ca6bed0/5DD5956B/t51.2885-19/s320x320/59440893_336666376926221_276408968295743488_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com" alt="">
+        <div style="display: flex; justify-content: space-evenly;">
+          <div style="width:150px">
+            <img class="pp" src="https://scontent-sin2-2.cdninstagram.com/vp/6f793dacb0901845be55c3624ca6bed0/5DD5956B/t51.2885-19/s320x320/59440893_336666376926221_276408968295743488_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com" alt="" style="width:100%; border-radius: 50%;">
+          </div>
           <div class="username">zachlavine8</div>
         </div>
 
-        <div class="ppc">
-          <img class="pp" src="https://scontent-sin2-2.cdninstagram.com/vp/6f793dacb0901845be55c3624ca6bed0/5DD5956B/t51.2885-19/s320x320/59440893_336666376926221_276408968295743488_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com" alt="">
+        <div style="display: flex; justify-content: space-evenly;">
+          <div style="width:150px">
+            <img class="pp" src="https://scontent-sin2-2.cdninstagram.com/vp/6f793dacb0901845be55c3624ca6bed0/5DD5956B/t51.2885-19/s320x320/59440893_336666376926221_276408968295743488_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com" alt="" style="width:100%; border-radius: 50%;">
+          </div>
           <div class="username">zachlavine8</div>
         </div>
       </div>
@@ -33,12 +37,18 @@ export default {
   },
   methods: {
     getInitialUser() {
-      console.log(this.$route.query.user)
-      console.log(this.$route.query.key)
-      console.log(this.$route.query.match)
+      const getSavedUsers = JSON.parse(localStorage.getItem('savedUsers'))
+      const user1 = getSavedUsers.savedUsers.find(user=>user.username === this.$route.query.match)
+
+      console.log(user1);
     }
   },
   mounted() {
+    const savedUsers = JSON.stringify({"savedUsers": [ {"username": "stef", "userImage": "http://www.stef.com", "ibmResult": {}}, {"username": "lutfi", "userImage": "http://www.lutfi.com", "ibmResult": {}}, {"username": "max", "userImage": "http://www.max.com", "ibmResult": {}}, {"username": "yonathan", "userImage": "http://www.yonathan.com", "ibmResult": {}}]})
+
+    localStorage.setItem('savedUsers', savedUsers)
+
+
     this.getInitialUser()
   },
 }
