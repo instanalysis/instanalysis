472bff0fbcb7e6ff0f2ac45fbfba559b2a11bc62
Max Tedjojuwono <maxpt750@gmail.com>
2019-07-30 18:46:21 +0700

    about to merge all

--- server/middlewares/errorhandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-  console.log('Error caught by error handler!');
-  console.log(err);
-  let status = err.status || 500;
-  let message = err.msg || 'Internal server error';
-  res.status(status).json({ message });
-}
