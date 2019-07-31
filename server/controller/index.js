c49f858854d99be0cb807517d0b43c7316ec0d3b
yploekito <yp.loekito@gmail.com>
2019-07-31 20:07:20 +0700

    settimeout start socket

--- server/controller/index.js
+++ server/controller/index.js
@@ -56,7 +56,7 @@ class analysisController {
                         totalLikes: likes
                     }
                 )
-            }, 1500)
+            }, 5000)
 
             // IBManalysis
             let personalityAnalysisResult = await personalityAnalysis(userData)
